48b624aab0dc559ad3d82eeacca982ece1787ac5
krishnapriya27051997-glitch <krishnapriya27051997@gmail.com>
2026-06-05 09:35:59 +0530

    Adding all files

---
+++ JStask.js
@@ -0,0 +1,3 @@
+let a = "banana";
+let result =a.split("").reverse().join("");
+console.log(result);
---
+++ demo.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 5; i++) {
+let row= "";
+for  (let j = 1; j <= 5-i; j++){
+ row += " ";
+}
+for (let k = 1; k<= 2*i-1; k++){
+ row += "*";    
+}
+console.log(row);
+
+}     
